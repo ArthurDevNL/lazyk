7de41a788f33460325b5ca9728180266df682599
Arthur Hemmer <47318376+AHemmerShift@users.noreply.github.com>
2024-02-22 20:43:14 +0000

    Update pyproject

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 ext_modules = [
     Pybind11Extension(
-        "lazyk",
+        "lazyk_decoding",
         sorted(glob("src/*.cpp"))
     ),
 ]
 
 setup(
-    name="lazyk",
+    name="lazyk-decoding",
     version="0.1.0",
     author="Arthur Hemmer",
-    author_email="arthur.hemmer@shift-technology.com",
+    author_email="arthurhemmer@hotmail.com",
     description="A Python library with a C++ implementation of the lazy-k algorithm",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
